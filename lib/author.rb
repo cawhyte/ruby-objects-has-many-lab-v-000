f9fc23a6f8b6da55b286ae6f36ac36289297f1de
Claribel Aguilar <claribel.aguilarwhyte@gmail.com>
2019-05-27 04:10:12 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -19,6 +19,11 @@ def add_post(post)
   post.author = self
 end
 
+def add_post_by_title(title)
+  post = Post.new(title)
+  post.author = self
+end
+
 def posts
   Post.all.select{|post| post.author == self}
 end
