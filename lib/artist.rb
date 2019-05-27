4ff06b426cc938de54ab2cb3c2144372de5ff740
Claribel Aguilar <claribel.aguilarwhyte@gmail.com>
2019-05-27 03:48:48 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,7 +18,7 @@ end
 
 def add_song_by_name(name)
   song = Song.new(name) #takes in an arguement for a song and then associates it with the song and artist
-  self.song << songs
+  self.songs << song
   song.artist = self
   @@song_count += 1
 end
