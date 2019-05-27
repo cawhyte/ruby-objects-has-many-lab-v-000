ec622c589d4a1e4e69c27ab43c75fee35b5d7edd
Claribel Aguilar <claribel.aguilarwhyte@gmail.com>
2019-05-27 03:41:12 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,19 @@
+require 'pry'
+
+class Artist
+  attr_accessor :name, :songs
+
+@@song_count = 0
+
+  def initialize(name) #allows access and stores artist name
+    @name = name
+    @songs = []
+  end
+
+def add_song(song)
+  self.songs << song #creates new instances of artist & adds it to the artist array of songs
+  song.artist = self
+  @@song_count += 1
+end
+
+end
