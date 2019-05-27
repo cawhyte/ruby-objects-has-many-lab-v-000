f5e2c9784e55b8c03946d861e992a8cb1e60b82c
Claribel Aguilar <claribel.aguilarwhyte@gmail.com>
2019-05-27 04:02:09 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -5,10 +5,18 @@ attr_accessor :name
 
 @@all = [] #=> allows for storing information in empty array
 
-def initialize(name)
+def initialize(name) #=> initializes name and creates instance of variable
   @name = name
   @@all << self
 end
 
+def self.all #=>creates instance of variable
+  @name = name
+@@all
+end
+
+def add_post(post)
+  post.author = self
+end
 
 end
